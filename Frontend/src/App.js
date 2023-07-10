4fc7292ff3e10438f345907c0d3ca87cbb873518
krishnajoshi27 <krishnajoshi316@gmail.com>
2023-07-09 23:09:26 -0400

    Added third party API

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,7 +7,7 @@ import Header from './Pages/Header'
 import Addcts from './Pages/Addcts'
 import Newsandannc from './Pages/Newsandannc'
 import Allcourses from './Pages/Allcourses'
-import Upcomeing from './Pages/Upcomeing'
+//import Upcomeing from './Pages/Upcomeing'
 import Viewcourses from './Pages/Viewcourses'
 import Signupstudent from './Pages/Signupstudent'
 import Setting from './Pages/Setting'
@@ -25,7 +25,6 @@ function App() {
         <Route path='/Addcts' element={<Addcts/>}></Route>
         <Route path='/Newsandannc' element={<Newsandannc/>}></Route>
         <Route path='/Allcourses' element={<Allcourses/>}></Route>
-        <Route path='/Upcomeing' element={<Upcomeing/>}></Route>
         <Route path='/Viewcourses' element={<Viewcourses/>}></Route>
         <Route path='/Setting' element={<Setting/>}></Route>
         <Route path='/Students' element={<Students/>}></Route>
